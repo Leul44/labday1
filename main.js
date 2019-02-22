0b346044aeae14b5e5e72de05a20cbf076894391
leul44 <leul441@hotmail.com>
2019-02-21 20:44:28 -0500

    created html, css , Javascript

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = prompt("whats your name")
+let classs = prompt("whats your class level")
+let movie = prompt("whats your favorate movie")
+
+confirm(`${name} your class level ${classs} your movie type is ${movie}`)
