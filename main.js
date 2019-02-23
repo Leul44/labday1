46b6987852114ca77f0e386566fbb00eab8cbdd6
leul44 <leul441@hotmail.com>
2019-02-23 09:57:32 -0500

    we add a paragraph in js

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ let classs = prompt("whats your class level")
 let movie = prompt("whats your favorate movie")
 
 confirm(`${name} your class level ${classs} your movie type is ${movie}`)
+
+let para = document. getElementById("para")
+
+para.innerText =`${name} your class level ${classs} your movie type is ${movie}`
