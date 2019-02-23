1aea7181c1f6e31f8c83747d468f549e8e092386
leul44 <leul441@hotmail.com>
2019-02-23 11:24:06 -0500

    we add a note

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let name = prompt("whats your name")
 let classs = prompt("whats your class level")
 let movie = prompt("whats your favorate movie")
 
+let para =document.getElementById('para')
+
+para.innerText = `${name} your class level ${classs} your movie type is ${movie}`
+
 confirm(`${name} your class level ${classs} your movie type is ${movie}`)
-
-let para = document. getElementById("para")
-
-para.innerText =`${name} your class level ${classs} your movie type is ${movie}`
